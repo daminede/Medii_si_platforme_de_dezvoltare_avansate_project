b566610207fa24de8db8892b80612d0208e9344a
daminede <108881810+daminede@users.noreply.github.com>
2025-03-31 18:12:56 +0300

    Add files via upload
    i first created a WinForms App by mistake and now that i saw i created a new project with a console app

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AnimalSounds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var animals = new List<Animal>
+            {
+                new Animal { Type = "Dog", Sound = "Bark" },
+                new Animal { Type = "Cat", Sound = "Meow" },
+                new Animal { Type = "Cow", Sound = "Moo" },
+                new Animal { Type = "Sheep", Sound = "Baa" },
+                new Animal { Type = "Duck", Sound = "Quack" }
+            };
+
+            foreach (var animal in animals)
+            {
+                Console.WriteLine($"{animal.Type} makes sound: {animal.Sound}");
+            }
+        }
+    }
+
+    class Animal
+    {
+        public string Type { get; set; }
+        public string Sound { get; set; }
+    }
+}
